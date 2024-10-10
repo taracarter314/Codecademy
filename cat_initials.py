fd41dea9b96b729224cb55ec0c0e00662c3590a0
taracarter314 <taracarter314@gmail.com>
2024-10-10 14:57:46 -0500

    cat_initials.py

---
+++ cat_initials.py
@@ -0,0 +1,37 @@
+#Tara Carter
+#I Like to Crochet
+
+#FIRST ATTEMPT
+print("T T T T T" + "    C C C")
+print("    T" + "      C       C")
+print("    T" + "      C")
+print("    T" + "      C")
+print("    T" + "      C")
+print("    T" + "      C       C")
+print("    T"+ "        C C C")
+
+#Second attempt after checking example code
+print(" CCC  TTTTT")
+print("C   C   T  ")
+print("C       T  ")
+print("C       T  ")
+print("C       T  ")
+print("C   C   T  ")
+print(" CCC    T  ")
+
+#Cat
+print("                              **    ")
+print("                             * *    ")
+print("             /\   /\       *  *     ")
+print("           *   * *   *    *  *      ")
+print("         *    @   @    *   *  *     ")
+print("        *       V       *   *  *    ")
+print("         *      U      *     *  *   ")
+print("           * * * * * *       *  *   ")
+print("         *            *      *  *   ")
+print("       *                *   *  *    ")
+print("      *      *    *      *  * *     ")
+print("      *     * *  * *     * * *      ")
+print("       *    * *  * *    * **        ")
+print("         *  * *  * *  *             ")
+print("             *    *                 ")
