433fc9126b78e7095a887017d9bb48a3fbf48f71
Tara Carter <taracarter314@gmail.com>
2024-10-15 23:08:14 -0500

    carlys_clippers.py

--- carlys_clippers.py
+++ carlys_clippers.py
@@ -6,8 +6,8 @@ last_week = [2, 3, 5, 8, 4, 4, 6, 2]
 
 total_price = 0
 
-for i in prices:
-  total_price += i
+for cost in prices:
+  total_price += cost
   average_price = total_price / len(prices) 
   print("Average Haircut Price:", average_price)
   new_price = [price - 5 for price in prices]
@@ -15,11 +15,11 @@ for i in prices:
 
   total_revenue = 0
   for i in range(len(hairstyles)):
-    total_revenue = prices[i]+last_week[i]
+    total_revenue = prices[i] + last_week[i]
     print("Total Revenue:", total_revenue)
 
 average_daily_revenue = total_revenue / 7
+print(average_daily_revenue)
 
 cuts_under_30 = [i for i in range(len(new_price)) if new_price[i] <= 30]
 print(cuts_under_30)
-
