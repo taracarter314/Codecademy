c644399ff4f62b110869bbbbc97da2d3b263a86e
Tara Carter <taracarter314@gmail.com>
2024-12-05 21:47:21 -0600

    custom_module.py

---
+++ custom_module.py
@@ -0,0 +1,2 @@
+def generate_time_travel_message(year, destination, cost):
+  return "Pack your bags! You're traveling to ", {destination}, "in the year", {year}, ". The cost of this trip will be $", {cost},"."
